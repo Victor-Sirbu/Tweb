b5297df25c53ed144340339872746977bc1ef42e
Malairau Dumitru <dumitru.malairau@isa.utm.md>
2026-02-20 12:20:08 +0200

    adaugare react route si stabilire routs la home/login

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./components/HomePage/HomePage";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 
-
 function App() {
-    return <ProfilePage />;
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
